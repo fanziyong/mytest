3a0c4799e75c32555b0e95e0a2308a40e76182c5
fanziyong <382423067@qq.com>
2018-11-13 00:42:58 +0800

    add

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fy
+ * Date: 12/11/18
+ * Time: 下午11:18
+ */
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fy
+ * Date: 12/11/18
+ * Time: 下午11:01
+ */
+
+phpinfo();
